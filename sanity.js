b668f550c18a10b47f7a2236cdd976c6e576ec2d
EldarMemetov <eldarm9347@gmail.com>
2025-05-20 15:55:46 +0200

    add

--- sanity.js
+++ sanity.js
@@ -1,11 +1,12 @@
-import sanityClient from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url';
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: '79cqv88x',
-  dataset: 'production',
+  projectId: "79cqv88x",
+  dataset: "production",
   useCdn: true,
-  apiVersion: '2023-05-19',
+  apiVersion: "2023-05-19",
+  useCdn: true,
 });
 
 const builder = imageUrlBuilder(client);
